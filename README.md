24ed1b20c63215bc4fb267aa2d75b84583f9bc78
kkhsckbk <kkhsckbk@naver.com>
2022-10-21 17:01:39 +0900

    wrap-up (108p)

---
+++ README.md
@@ -0,0 +1,8 @@
+## Introduction
+This project is to learn SpringBoot with SpringFramework with old-time Java knowledge.
+Remarkable comments from textbook will reside in each code part.
+
+<b>Technology stacks</b>: Java, SpringBoot, JPA, et cetera.
+
+Till being familar with the [markdowns](https://www.jetbrains.com/help/idea/markdown.html#f19a1cc2), let it be...
+
---
+++ src/main/java/org/example/caleb/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package org.example.caleb.springboot.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.caleb.springboot.domain.posts.PostsRepository;
+import org.example.caleb.springboot.web.dto.PostsSaveRequestDto;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
